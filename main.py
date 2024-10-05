938e832394c489e98040ec960c0f31d59d634a9c
vitcool@ukr.net <vitcool@ukr.net>
2024-10-05 14:10:43 +0300

    BT-0: black is added for code formatting

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import os
 load_dotenv()
 
 # Access the variables
-device_key = os.getenv('DEVICE_KEY')
-login_user = os.getenv('LOGIN_USER')
-login_password = os.getenv('LOGIN_PASSWORD')
+device_key = os.getenv("DEVICE_KEY")
+login_user = os.getenv("LOGIN_USER")
+login_password = os.getenv("LOGIN_PASSWORD")
 
 print(f"Using device key: {device_key}")
 print(f"Using login user: {login_user}")
