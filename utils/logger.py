b6e05d3a4046d364992b8dc78176eda455ed393f
Vitalii Kulyk <vitcool@ukr.net>
2024-12-06 23:29:41 +0200

    Bt 16 logger amendments (#8)
    * BT-17: logger setup update
    
    * BT-17: logger config update

--- utils/logger.py
+++ utils/logger.py
@@ -1,13 +1,33 @@
-import logging
 import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 def setup_logging():
     is_dev_env = os.getenv("ENV") == "dev"
     folder_path = 'logs'
     os.makedirs(folder_path, exist_ok=True)
     
-    logging.basicConfig(
+    log_handler = TimedRotatingFileHandler(
         filename=f'{folder_path}/log.txt',
-        level=logging.DEBUG if is_dev_env else logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
+        when="midnight",
+        interval=1,
+        backupCount=7,  # Keeps logs of the last 7 days
+        encoding="utf-8"
     )
+    
+    # Define the log format
+    log_format = '%(asctime)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(log_format)
+    log_handler.setFormatter(formatter)
+    
+    # Set the logging level for the handler
+    log_handler.setLevel(logging.DEBUG if is_dev_env else logging.INFO)
+    
+    # Get the root logger
+    root_logger = logging.getLogger()
+    
+    # Add the handler to the root logger
+    root_logger.addHandler(log_handler)
+    
+    # Set the logging level for the root logger
+    root_logger.setLevel(logging.DEBUG if is_dev_env else logging.INFO)
